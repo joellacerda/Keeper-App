e2d609b764e27da70bc840f1ed86ca7b333087b1
Joel Lacerda <joellacerdaol@gmail.com>
2023-02-24 19:01:29 -0300

    feat: refactor notes to add more elements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import { notes } from "../notes";
 
 export default function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((entry) => (
+        <Note key={entry.id} title={entry.title} content={entry.content} />
+      ))}
       <Footer />
     </div>
   );
