ab058773e0e88d6e3613988992d2d0309b587483
Joel Lacerda <joellacerdaol@gmail.com>
2023-02-25 08:06:17 -0300

    feat: make header not selectable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header>
-      <h1>Note Pad</h1>
+      <h1 className="not-selectable">Note Pad</h1>
     </header>
   );
 }
